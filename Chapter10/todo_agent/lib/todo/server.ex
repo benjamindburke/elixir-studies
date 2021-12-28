5fb3581440b97d565ca453d64fdac3d8d92f62e3
Ben Burke <benjamindburke@gmail.com>
2021-12-28 18:02:30 -0500

    init project by cp Chapter10/todo_metrics (10.2.4)

--- Chapter10/todo_agent/lib/todo/server.ex
+++ Chapter10/todo_agent/lib/todo/server.ex
@@ -1,7 +1,7 @@
 # Todo Server [dynamic worker]
 # This module manipulates and queries a client's to-do list
 defmodule Todo.Server do
-  use GenServer, restart: :temporary
+  use Agent, restart: :temporary
 
   # ---------
   # Interface functions
@@ -10,87 +10,69 @@ defmodule Todo.Server do
   # CREATE
   # ---------
   @spec add_entry(pid, map) :: :ok
-  def add_entry(pid, entry) do
-    GenServer.cast(pid, {:add_entry, entry})
+  def add_entry(pid, new_entry) do
+    Agent.cast(
+      pid,
+      fn {name, todo_list} ->
+      new_list = Todo.List.add_entry(todo_list, new_entry)
+      Todo.Database.store(name, new_list)
+      {name, new_list}
+      end
+    )
   end
 
   # READ
   # ---------
   @spec entries(pid, any) :: list
   def entries(pid, query) do
-    GenServer.call(pid, {:entries, query})
+    Agent.get(
+      pid,
+      fn {_name, todo_list} -> Todo.List.entries(todo_list, query) end
+    )
   end
 
   # UPDATE
   # ---------
   @spec update_entry(pid, map) :: :ok
   def update_entry(pid, %{} = new_entry) do
-    GenServer.cast(pid, {:update_entry, new_entry})
+    Agent.cast(
+      pid,
+      fn {name, todo_list} ->
+        new_list = Todo.List.update_entry(todo_list, new_entry)
+        Todo.Database.store(name, new_list)
+        {name, new_list}
+      end
+    )
   end
 
   # DELETE
   # ---------
   @spec delete_entry(pid, integer) :: :ok
   def delete_entry(pid, id) do
-    GenServer.cast(pid, {:delete_entry, id})
+    Agent.cast(
+      pid,
+      fn {name, todo_list} ->
+        new_list = Todo.List.delete_entry(todo_list, id)
+        Todo.Database.store(name, new_list)
+        {name, new_list}
+      end
+    )
   end
 
   # ---------
   # DynamicSupervisor hook functions
   # ---------
 
-  @spec start_link(charlist, list | nil) :: {:error, any} | {:ok, pid}
   def start_link(name, entries \\ []) do
-    IO.puts("Starting to-do server for #{name}.")
-    GenServer.start_link(__MODULE__, {name, entries}, name: via_tuple(name))
+    Agent.start_link(
+      fn ->
+        IO.puts("Starting to-do server for #{name}.")
+        {name, Todo.Database.get(name) || Todo.List.new(entries)}
+      end
+    )
   end
 
   defp via_tuple(name) do
     Todo.ProcessRegistry.via_tuple({__MODULE__, name})
-  end
-
-  # ---------
-  # GenServer hook functions
-  # ---------
-
-  @impl GenServer
-  @spec init({charlist, list}) :: {:ok, {charlist, Todo.List}}
-  def init({name, entries}) do
-    {:ok, {name, Todo.Database.get(name) || Todo.List.new(entries)}}
-    # send(self(), :real_init) # uncomment to hook into handle_info(:real_init, ...) callback
-  end
-
-  # READ
-  # ---------
-  @impl GenServer
-  def handle_call({:entries, query}, _, {name, todo_list}) do
-    {:reply, Todo.List.entries(todo_list, query), {name, todo_list}}
-  end
-
-  # CREATE
-  # ---------
-  @impl GenServer
-  def handle_cast({:add_entry, new_entry}, {name, todo_list}) do
-    new_list = Todo.List.add_entry(todo_list, new_entry)
-    Todo.Database.store(name, new_list)
-    {:noreply, {name, new_list}}
-  end
-
-  # UPDATE
-  # ---------
-  @impl GenServer
-  def handle_cast({:update_entry, new_entry}, {name, todo_list}) do
-    new_list = Todo.List.update_entry(todo_list, new_entry)
-    Todo.Database.store(name, new_list)
-    {:noreply, {name, new_list}}
-  end
-
-  # DELETE
-  # ---------
-  @impl GenServer
-  def handle_cast({:delete_entry, id}, {name, todo_list}) do
-    new_list = Todo.List.delete_entry(todo_list, id)
-    Todo.Database.store(name, new_list)
-    {:noreply, {name, new_list}}
   end
 end
---
+++ Chapter10/todo_cache_expiry/lib/todo/server.ex
@@ -0,0 +1,96 @@
+# Todo Server [dynamic worker]
+# This module manipulates and queries a client's to-do list
+defmodule Todo.Server do
+  use GenServer, restart: :temporary
+
+  # ---------
+  # Interface functions
+  # ---------
+
+  # CREATE
+  # ---------
+  @spec add_entry(pid, map) :: :ok
+  def add_entry(pid, entry) do
+    GenServer.cast(pid, {:add_entry, entry})
+  end
+
+  # READ
+  # ---------
+  @spec entries(pid, any) :: list
+  def entries(pid, query) do
+    GenServer.call(pid, {:entries, query})
+  end
+
+  # UPDATE
+  # ---------
+  @spec update_entry(pid, map) :: :ok
+  def update_entry(pid, %{} = new_entry) do
+    GenServer.cast(pid, {:update_entry, new_entry})
+  end
+
+  # DELETE
+  # ---------
+  @spec delete_entry(pid, integer) :: :ok
+  def delete_entry(pid, id) do
+    GenServer.cast(pid, {:delete_entry, id})
+  end
+
+  # ---------
+  # DynamicSupervisor hook functions
+  # ---------
+
+  @spec start_link(charlist, list | nil) :: {:error, any} | {:ok, pid}
+  def start_link(name, entries \\ []) do
+    IO.puts("Starting to-do server for #{name}.")
+    GenServer.start_link(__MODULE__, {name, entries}, name: via_tuple(name))
+  end
+
+  defp via_tuple(name) do
+    Todo.ProcessRegistry.via_tuple({__MODULE__, name})
+  end
+
+  # ---------
+  # GenServer hook functions
+  # ---------
+
+  @impl GenServer
+  @spec init({charlist, list}) :: {:ok, {charlist, Todo.List}}
+  def init({name, entries}) do
+    {:ok, {name, Todo.Database.get(name) || Todo.List.new(entries)}}
+    # send(self(), :real_init) # uncomment to hook into handle_info(:real_init, ...) callback
+  end
+
+  # READ
+  # ---------
+  @impl GenServer
+  def handle_call({:entries, query}, _, {name, todo_list}) do
+    {:reply, Todo.List.entries(todo_list, query), {name, todo_list}}
+  end
+
+  # CREATE
+  # ---------
+  @impl GenServer
+  def handle_cast({:add_entry, new_entry}, {name, todo_list}) do
+    new_list = Todo.List.add_entry(todo_list, new_entry)
+    Todo.Database.store(name, new_list)
+    {:noreply, {name, new_list}}
+  end
+
+  # UPDATE
+  # ---------
+  @impl GenServer
+  def handle_cast({:update_entry, new_entry}, {name, todo_list}) do
+    new_list = Todo.List.update_entry(todo_list, new_entry)
+    Todo.Database.store(name, new_list)
+    {:noreply, {name, new_list}}
+  end
+
+  # DELETE
+  # ---------
+  @impl GenServer
+  def handle_cast({:delete_entry, id}, {name, todo_list}) do
+    new_list = Todo.List.delete_entry(todo_list, id)
+    Todo.Database.store(name, new_list)
+    {:noreply, {name, new_list}}
+  end
+end
