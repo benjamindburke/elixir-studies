290c644be939aa6fdeb65d5e82a8434c29f0cd98
Ben Burke <benjamindburke@gmail.com>
2021-12-28 21:46:22 -0500

    init key_value module

--- Chapter10/key_value/lib/key_value.ex
+++ Chapter10/key_value/lib/key_value.ex
@@ -1,18 +1,30 @@
 defmodule KeyValue do
-  @moduledoc """
-  Documentation for `KeyValue`.
-  """
+  use GenServer
 
-  @doc """
-  Hello world.
+  @impl GenServer
+  def init(_) do
+    {:ok, %{}}
+  end
 
-  ## Examples
+  def start_link do
+    GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
+  end
 
-      iex> KeyValue.hello()
-      :world
+  def put(key, value) do
+    GenServer.cast(__MODULE__, {:put, key, value})
+  end
 
-  """
-  def hello do
-    :world
+  def get(key) do
+    GenServer.call(__MODULE__, {:get, key})
+  end
+
+  @impl GenServer
+  def handle_cast({:put, key, value}, store) do
+    {:noreply, Map.put(store, key, value)}
+  end
+
+  @impl GenServer
+  def handle_call({:get, key}, _, store) do
+    {:reply, Map.get(store, key), store}
   end
 end
