c49b78ebae280ac0de1b70a30405ee5ee9018566
Ben Burke <benjamindburke@gmail.com>
2022-01-02 18:33:04 -0500

    distributed database, data replication

--- Chapter12/todo_distributed/lib/todo/database.ex
+++ Chapter12/todo_distributed/lib/todo/database.ex
@@ -6,18 +6,37 @@ defmodule Todo.Database do
   # Interface functions
   # ---------
 
+  @doc """
+    store/2 performs an RPC multicall to all Todo.Database nodes,
+    calling each Todo.Database node's store_local/2 function,
+    thereby replicating all data to all database nodes.
+  """
   def store(key, data) do
-    :poolboy.transaction(
-      __MODULE__,
-      fn worker_pid -> Todo.DatabaseWorker.store(worker_pid, key, data) end
-    )
+    {_results, bad_nodes} =
+      :rpc.multicall(
+        __MODULE__,
+        :store_local,
+        [key, data],
+        :timer.seconds(5)
+      )
+
+    Enum.each(bad_nodes, &IO.puts("Store failed on node #{&1}"))
+  end
+
+  @doc """
+    store_local/2 stores the {key, data} pair in a
+    local binary file at the file path database_folder/node/key
+  """
+  def store_local(key, data) do
+    :poolboy.transaction(__MODULE__, fn worker_pid ->
+      Todo.DatabaseWorker.store(worker_pid, key, data)
+    end)
   end
 
   def get(key) do
-    :poolboy.transaction(
-      __MODULE__,
-      fn worker_pid -> Todo.DatabaseWorker.get(worker_pid, key) end
-    )
+    :poolboy.transaction(__MODULE__, fn worker_pid ->
+      Todo.DatabaseWorker.get(worker_pid, key)
+    end)
   end
 
   # ---------
@@ -26,9 +45,14 @@ defmodule Todo.Database do
 
   def child_spec(_) do
     db_settings = Application.fetch_env!(:todo, :database)
-    db_folder = Keyword.fetch!(db_settings, :folder)
+
+    # Node name is used to determine the database folder. This allows us to
+    # start multiple nodes form the same folders, and data will not clash.
+    [name_prefix, _] = "#{node()}" |> String.split("@")
+    db_folder = "#{Keyword.fetch!(db_settings, :folder)}/#{name_prefix}/"
 
     File.mkdir_p!(db_folder)
+
     :poolboy.child_spec(
       __MODULE__,
       [
